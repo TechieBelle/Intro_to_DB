25cf1f1abfc7bba9d4c895da82814ae71aabbfbb
Folashade Bello <folashadebello.b@gmail.com>
2025-07-02 13:10:48 +0100

    corrected

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 
-CREATE TABLE Books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id PRIMARY KEY,
     title VARCHAR(130),
     price DOUBLE,
@@ -10,26 +10,26 @@ CREATE TABLE Books (
 );
 
 
-CREATE TABLE Authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id PRIMARY KEY,
     author_name VARCHAR(215)
 
 );
 
-CREATE TABLE Customers(
+CREATE TABLE IF NOT EXISTS Customers(
     customer_id  PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
-CREATE TABLE Orders(
+CREATE TABLE IF NOT EXISTS Orders(
     order_id PRIMARY KEY,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
 
-CREATE TABLE Order_Details(
+CREATE TABLE IF NOT EXISTS Order_Details(
     orderdetailid  PRIMARY KEY,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id),
