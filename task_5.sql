88c7b40b911c8a298311c7f11d654a3da7205425
Folashade Bello <folashadebello.b@gmail.com>
2025-07-02 15:15:21 +0100

    task 5

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+USE DATABASE alx_book_store;
+
+INSERT INTO Books Customers(customer_id, customer_name, email, address) 
+VALUES (
+    1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.', 1
+)
