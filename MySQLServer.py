dd97921fb01b2eb6f7903cc71d8fd9824208f46c
Folashade Bello <folashadebello.b@gmail.com>
2025-07-02 14:37:32 +0100

    corrected

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,26 +1,36 @@
 import mysql.connector
+
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-connector = mysql.connector.connect(
-    host = os.getenv("DB_HOST"),
-    user = os.getenv("DB_USER"),   
-    password = os.getenv("DB_PASSWORD"),
-    database = os.getenv("DB_NAME")
-)
-#connect to the database
-cursor = connector.cursor()
+
+
 
 
 def create_database():
     try:
+        connector = mysql.connector.connect(
+            host = os.getenv("DB_HOST"),
+            user = os.getenv("DB_USER"),   
+            password = os.getenv("DB_PASSWORD")
+            # database = os.getenv("DB_NAME")
+        )
+        #connect to the database
+        cursor = connector.cursor()
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database created successfully.")
-    except:
-        print("Error creating database.")
+        cursor.close()
+        connector.close()
+        print("Database connection closed.")
+    except Exception as e:
+        print("Error:", e)
 
 
-connector.commit()
-connector.close()
+
+
+if __name__ == "__main__":
+    create_database()
+  
+
